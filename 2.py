9a6298f4f3fd9dfa799e63e72c100aec3c6e5a31
vovvoy <ormonbaev.marsel@gmail.com>
2019-06-15 17:35:06 +0600

    task2 part3

---
+++ 2.py
@@ -0,0 +1,21 @@
+your_list = []
+list_size = int(input())
+for x in range (list_size):
+    volue = int(input())
+    your_list.append(volue)
+
+step = int(input())
+
+if step < 0:
+    your_list.reverse()
+print (your_list)
+s = 0
+for x in range (step):
+    your_list.append(your_list[sx)
+    your_list.remove(x)
+    s += 1
+
+if step < 0:
+    your_list.reverse()   
+print (your_list)
+    
